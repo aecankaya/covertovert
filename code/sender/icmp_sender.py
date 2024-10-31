513d6cbe14ee8887fca2d97c3236236c419d768f
aecankaya <aecankaya10@gmail.com>
2024-10-31 21:36:32 +0300

    comment changed

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -3,11 +3,9 @@ from scapy.all import IP,ICMP,send
 # Implement your ICMP sender here
 
 def send_icmp():
-    # Send an ICMP packet to the broadcast address
     packet = IP(dst="172.18.255.255", ttl=1) / ICMP()
     send(packet)
 
-# Send an ICMP packet to the broadcast address
 if __name__ == "__main__":
     send_icmp()
 
